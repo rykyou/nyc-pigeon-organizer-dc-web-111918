1fcf505296d3f561c505fd27413c8044b187c2af
Robin Kim <rkim1214@gmail.com>
2018-11-04 22:07:46 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attribute_array|
+    attribute_array[1].each do |k, v|
+      v.each do |pigeon|
+        if new_hash[pigeon]
+          new_hash[pigeon][attribute_array[0]] << k.to_s
+        else
+          new_hash[pigeon] = {:color => [k.to_s], :gender => [], :lives => []}
+        end
+      end 
+    end
+  end
+  new_hash
 end
