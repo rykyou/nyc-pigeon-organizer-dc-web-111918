9121cc5fb5909971a67b190a2004614bdbb9c6f3
Robin Kim <rkim1214@gmail.com>
2018-11-04 21:55:03 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -30,6 +30,7 @@ def nyc_pigeon_organizer(data)
           new_hash[pigeon] = {:color => [k.to_s]}
           new_hash[pigeon] = :gender
           new_hash[pigeon] = :lives
+          binding.pry
         end
       end 
     end
